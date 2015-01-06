6c2af814adb6f30affbbb048d3f5c8174696a28a
Rubin Thomas <rubin@rubinthomas.com>
2015-01-06 16:14:33 +1300

    Changes to Code
    Added heaps of changes to code. To support Taxonomy Mapping, a new sub
    module has been created to handle tags and categories from the CMIS
    Repository. Primarily present in Alfresco.

---
+++ cmis_taxonomy/cmis_vocab.sql
@@ -0,0 +1,11 @@
+INSERT INTO `cmis_vocab` (`cmis_vid`, `vid`, `cmis_vname`, `cmis_repositoryId`, `cmis_objectId`, `cmis_exclude`, `timestamp`) VALUES
+(1, 1, 'Tags', 'default', 'tag:tag-root', 0, 0),
+(2, 10, 'Languages', 'default', '082034b1-e271-44f7-946d-cdaeba93b84b', 1, 0),
+(3, 11, 'Regions', 'default', '1c5540df-4da1-4844-892c-2306c2bd905c', 1, 0),
+(4, 2, 'Document Classification', 'default', '8368c691-2dae-478f-b0d1-9f77eef9ff10', 0, 0),
+(5, 5, 'Departments', 'default', 'b68e6d58-8a2f-44e8-95fc-a719b525daf9', 0, 0),
+(6, 8, 'Audience', 'default', '2eeb7ee6-6a2d-496f-a52e-59244811c5ae', 0, 0),
+(7, 4, 'Brands', 'default' , 'b224a189-5b22-4c75-aaf3-24f9a81abead', 0, 0),
+(8, 6, 'Product Category', 'default' , '9ef1f399-3add-41c7-80d4-0262e191b17a', 0, 0),
+(9,7, 'Topics', 'default' , 'f401f26a-3d85-4f73-afc9-7f91b8e1503c', 0, 0),
+(10,3, 'Site Relevance', 'default' , 'c1b9cb2a-aa11-4232-ba1e-1f4236d77cf6', 0, 0);
