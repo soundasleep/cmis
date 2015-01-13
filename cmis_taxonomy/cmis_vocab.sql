b23e79ec4c9a68e1112da703b2d52541c500c230
Rubin Thomas <rubin@rubinthomas.com>
2015-01-13 16:50:10 +1300

    SQL for Vocabulary Mapping
    Interim SQL for Vocabulary Mapping

---
+++ cmis_taxonomy/cmis_vocab.sql
@@ -0,0 +1,4 @@
+INSERT INTO `cmis_vocab` (`cmis_vid`, `vid`, `cmis_vname`, `cmis_repositoryId`, `cmis_objectId`, `cmis_exclude`, `timestamp`) VALUES
+(1, 1, 'Tags', 'default', 'tag:tag-root', 0, 0),
+(2, 10, 'Languages', 'default', '082034b1-e271-44f7-946d-cdaeba93b84b', 1, 0),
+(3, 11, 'Regions', 'default', '1c5540df-4da1-4844-892c-2306c2bd905c', 1, 0);
